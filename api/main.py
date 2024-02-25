2553101da2958303ccb5a33490838042ee7ae02d
VergilX <abhinand.dmanoj@gmail.com>
2024-02-25 13:19:52 +0530

    Working backend

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 from fastapi import FastAPI, File, UploadFile
+import mysql.connector
 import numpy as np
 from PIL import Image
 import tensorflow as tf
@@ -10,6 +11,16 @@ app = FastAPI()
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 MODEL_VERSION = 1
 MODEL = tf.keras.models.load_model(f'../models/{MODEL_VERSION}')
+
+# DATABASE CONFIG
+DB_CONFIG = {
+    'user': '<username>',
+    'password': '<password>',
+    'host': '<host>',
+    'database': '<dbname>',
+    'raise_on_warnings': True
+}
+TABLE = "<tablename>"
 
 
 # Ping
@@ -39,10 +50,32 @@ async def predict(
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
 
+    # Getting database data
+    query = f"SELECT * FROM {TABLE} WHERE Name=\"{predicted_class}\""
+
+    cursor.execute(query)
+    result = cursor.fetchone()
+    if result in [None, ""]:
+        print("Healthy plant")
+        return "Healthy"
+
+    id, name, causes, symptoms, treatment = result
+    # print(id, name, causes, symptoms, treatment)
+
     return {
-            'class': predicted_class,
-            'confidence': float(confidence)
+        'class': predicted_class,
+        'confidence': float(confidence),
+        'name': name,
+        'causes': causes,
+        'symptoms': symptoms,
+        'treatment': treatment
     }
 
 if __name__ == "__main__":
+    # Database connection
+    cnx = mysql.connector.connect(**DB_CONFIG)
+    cursor = cnx.cursor()
+
     uvicorn.run(app, host='localhost', port=8000)
+
+    cnx.close()
